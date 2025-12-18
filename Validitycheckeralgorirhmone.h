5cc262093fcdeaaeb1b4d7a3b1da418e7bafe60c
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-18 15:39:50 +0100

    Create Validitycheckeralgorirhmone.h

---
+++ Validitycheckeralgorirhmone.h
@@ -0,0 +1,8 @@
+//to check validoty of a sub clister ans then do
+//bootstrapi'g a lottle bit, we may need some strong ciphers
+//for ill defined samoling. bit with 30 weaknciphers,
+//we cannot sample every probes. so its better to use hemisphere samplo'g
+//on a differential formula that is an intergal. so this is like full sphere sampling on a 3d probes.
+//to use 6d validity, we nees to set 6 fluid dynamics if E can
+//no matter there first meaning. and we need to force using 6d maths or physics cpp code lines 
+//the result will be a non valid bit smooth valie for other integrals for other files that use next different algorithms.
